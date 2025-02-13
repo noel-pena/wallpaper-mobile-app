ee6fab954430947cfd28235c1e16b50448c2a9b7
noel-pena <azulu88@hotmail.com>
2025-02-12 20:30:54 -0500

    debugging api call

--- backend/src/main/kotlin/model/Wallpaper.kt
+++ backend/src/main/kotlin/model/Wallpaper.kt
@@ -3,13 +3,26 @@ package org.example.model
 import kotlinx.serialization.Serializable
 
 @Serializable
+data class Thumbs(
+    val large: String,
+    val original: String,
+    val small: String
+)
+
+@Serializable
 data class WallpaperData(
+    val id: String,
+    val url: String,
     val path: String,
+    val thumbs: Thumbs
 )
 
 @Serializable
 data class WallhavenWallpaperData(
+    val id: String,
+    val url: String,
     val path: String,
+    val thumbs: Thumbs
 )
 
 @Serializable
