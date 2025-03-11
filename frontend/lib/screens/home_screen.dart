101c53960946f79f08c8fce8aa965b8d146120e1
noel-pena <azulu88@hotmail.com>
2025-03-10 20:55:55 -0400

    beginning to design the refresh button

--- frontend/lib/screens/home_screen.dart
+++ frontend/lib/screens/home_screen.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import '../widgets/custom_app_bar.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreen();
-  }
+}
 
 class _HomeScreen extends State<HomeScreen> {
   @override
@@ -23,10 +24,15 @@ class _HomeScreen extends State<HomeScreen> {
       body: Stack(
         children: [
           Align(
-              alignment: Alignment(1, 1),
-              child: ElevatedButton(
-                  onPressed: (){},
-                  child: Text('Render')
+            alignment: Alignment(0, 0.8),
+            child: Icon(
+              Icons.replay_circle_filled,
+              size: 50,
+              color: Colors.black26,
+            )
+              .animate().rotate(
+                duration: const Duration(seconds: 1),
+                curve: Curves.linear,
               )
           )],
       ),
