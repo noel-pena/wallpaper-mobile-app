b56a8ffbad42b8ed66dc125d6938fe5dea8357bd
noel-pena <azulu88@hotmail.com>
2025-03-13 21:15:35 -0400

    added animation for refresh button

--- frontend/lib/screens/home_screen.dart
+++ frontend/lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'dart:math' as math;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,25 +9,42 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreen();
 }
 
-class _HomeScreen extends State<HomeScreen> {
+class _HomeScreen extends State<HomeScreen> with SingleTickerProviderStateMixin{
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+        vsync: this,
+        duration: const Duration(seconds: 1),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          Placeholder(),
+          const Placeholder(),
           Align(
-            alignment: Alignment(-0.25, 0.75),
-            child: IconButton(
-              iconSize: 50,
-              onPressed: (){},
-              color: Colors.black26,
-              icon: const Icon(Icons.replay_circle_filled),
-            )
-              .animate().rotate(
-                duration: const Duration(milliseconds: 500),
-                curve: Curves.linear,
-              )
+            alignment: const Alignment(-0.25, 0.75),
+              child: IconButton(
+                iconSize: 50,
+                onPressed: () {
+                  _animationController
+                    ..reset()
+                    ..forward();
+                },
+                color: Colors.black26,
+                icon: const Icon(Icons.replay_circle_filled),
+              ).animate(controller: _animationController, autoPlay: false).rotate(begin: 0, end: 4 * math.pi),
           ),
           Align(
             alignment: Alignment(0.25, 0.75),
