027dffcbde047d44f50c3b22302d0bceff4eb717
noel-pena <azulu88@hotmail.com>
2025-03-21 17:07:57 -0400

    completed api call function

--- frontend/lib/screens/home_screen.dart
+++ frontend/lib/screens/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
-import 'package:http/http.dart' as http;
+import 'package:frontend/services/wallpaper_service.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,16 +9,29 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreen();
 }
 
-class _HomeScreen extends State<HomeScreen> with SingleTickerProviderStateMixin{
-  static const address = 'http://localhost:8080/api/wallpapers/random';
+class _HomeScreen extends State<HomeScreen> with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
   bool isLiked = false;
+  final WallpaperService _wallpaperService = WallpaperService();
+  dynamic _wallpaperData;
 
   @override
   void initState() {
     super.initState();
     _fetchWallpaper();
     _animationController = AnimationController(vsync: this);
+  }
+
+  Future _fetchWallpaper() async {
+    try {
+      final data = await _wallpaperService.fetchWallpaper();
+      setState(() {
+        _wallpaperData = data;
+      });
+      print("aqui $_wallpaperData");
+    } catch (error) {
+      throw('Error: $error');
+    }
   }
 
   @override
@@ -38,35 +51,32 @@ class _HomeScreen extends State<HomeScreen> with SingleTickerProviderStateMixin{
             child: IconButton(
               iconSize: 50,
               onPressed: () {
+                _fetchWallpaper();
                 _animationController
                   ..reset()
                   ..forward();
               },
               color: Colors.black26,
               icon: const Icon(Icons.replay_circle_filled),
-            ).animate(controller: _animationController, autoPlay: false).rotate(begin: 0, end: -2, duration: Duration(seconds: 1)),
+            ).animate(controller: _animationController, autoPlay: false).rotate(
+                begin: 0, end: -2, duration: Duration(seconds: 1)),
           ),
           Align(
-            alignment: Alignment(0.25, 0.75),
-            child: IconButton(
-              iconSize: 50,
-              onPressed: (){
-                setState(() {
-                  isLiked = !isLiked;
-                });
-              },
-              color: isLiked ? Colors.redAccent : null,
-              icon: const Icon(Icons.favorite),
-            )
+              alignment: Alignment(0.25, 0.75),
+              child: IconButton(
+                iconSize: 50,
+                onPressed: () {
+                  _fetchWallpaper();
+                  setState(() {
+                    isLiked = !isLiked;
+                  });
+                },
+                color: isLiked ? Colors.redAccent : null,
+                icon: const Icon(Icons.favorite),
+              )
           )
         ],
       ),
     );
-  }
-
-  Future _fetchWallpaper() async {
-    final Uri url = Uri.parse(address);
-    final response = await http.get(url);
-    print(response.body);
   }
 }
---
+++ frontend/lib/services/wallpaper_service.dart
@@ -0,0 +1,33 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import 'dart:io' show Platform;
+
+class WallpaperService {
+  Future <dynamic> fetchWallpaper() async {
+    try {
+      String address;
+
+      if (kIsWeb) {
+        address = 'http://localhost:8080/api/wallpapers/random';
+      } else if (Platform.isAndroid) {
+        address = 'http://10.0.2.2:8080/api/wallpapers/random';
+      } else if (Platform.isIOS) {
+        address = 'http://127.0.0.1:8080/api/wallpapers/random';
+      } else {
+        address = 'http://localhost:8080/api/wallpapers/random';
+      }
+
+      final Uri url = Uri.parse(address);
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        return json.decode(response.body);
+      } else {
+        throw Exception("Failed to load wallpaper: ${response.statusCode}");
+      }
+    } catch (error) {
+      throw Exception("Could not reach call: $error");
+    }
+  }
+}
